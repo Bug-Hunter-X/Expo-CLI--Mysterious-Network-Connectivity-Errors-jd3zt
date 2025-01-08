fdcfd0b05a18dde2d04fd3b85cccfb724d763acf
BugHunter-X <askzenai@gmail.com>
2025-01-08 06:26:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo CLI and occurs when you try to run your app on a device or simulator and encounter an error related to network requests or connectivity. The error message itself might not be very descriptive, and you might see issues with fetching data or connecting to any services.
